55b1064ce2c409b5d4920e075a953387c8f72973
Yusman Kamaleri <ybkamaleri@gmail.com>
2020-03-26 20:11:23 +0100

    get flat detail

--- R/get_finn.R
+++ R/get_finn.R
@@ -8,9 +8,9 @@
 get_finn <- function(html){
 
     if (missing(html)) {
-    stop("'html' must be provided",
-         call. = FALSE)
-  }
+        stop("'html' must be provided",
+             call. = FALSE)
+    }
 
     ## Get finn koder
     allCode <- rvest::html_nodes(allFinn, css = c("div .ads__unit__content h2 a")) %>%
@@ -18,4 +18,26 @@ get_finn <- function(html){
         stringi::stri_extract(regex = "[^.*\\=]\\d+")
 
     return(allCode)
+}
+
+
+#' Get detail for a coded appartment
+#'
+#' @param code The code for a specific appartment or house
+#'
+#' @export
+
+get_flat <- function(code){
+
+    if (missing(x)) {
+        stop("'code' must be provided",
+             call. = FALSE)
+    }
+
+    boligHtml <- "https://www.finn.no/realestate/homes/ad.html?finnkode="
+    boKode <- paste0(boligHtml, code)
+    bolig <- xml2::read_html(boKode)
+
+    return(bolig)
+
 }

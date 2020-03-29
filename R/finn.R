0d566c971320ecc7c30955880a78e8f98ea099b0
Yusman Kamaleri <ybkamaleri@gmail.com>
2020-03-30 00:20:27 +0200

    Create data.table output

--- R/finn.R
+++ R/finn.R
@@ -10,6 +10,41 @@ finn <- function(area = 1:5, pmin = 3, pmax = 5, add = NULL, ...){
     ## Get html
     finnHTML <- get_html(area = area, pmin = pmin, pmax = pmax, add = add)
 
-    ## Finn codes
+    ## List of Finn codes for the specified parameters
     finnCodes <- get_finn(finnHTML)
+
+
+    finnTabel <- list()
+
+    for (i in 1:length(finnCodes)){
+        ## Detail info for a selected flat
+        aptCode <- get_flat(finnCodes[i])
+
+        ## Address
+        aptAdd <- get_add(aptCode)
+
+        ## price
+        aptPrice <- get_price(aptCode)
+
+        ## type
+        aptType <- get_type(aptCode)
+
+        ## ownership
+        aptForm <- get_form(aptCode)
+
+        ## postcode
+        aptPost <- get_add(aptCode, postcode = TRUE)
+
+        tempTab <- list(price = aptPrice,
+                        type = aptType,
+                        form = aptForm,
+                        address = aptAdd,
+                        postcode = aptPost)
+
+        finnTabel[[i]] <- tempTab
+    }
+
+    finnTabel <- data.table::rbindlist(finnTabel)
+
+    return(aptAdd)
 }
